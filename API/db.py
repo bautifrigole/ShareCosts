6b58060c47a057bad74261e7e11ce29987380cce
facu Gaviola <facundo.gaviola@gmail.com>
2023-02-22 00:10:57 -0300

    Conexion a base de datos

---
+++ API/db.py
@@ -0,0 +1,52 @@
+import mariadb
+import sys
+
+def connect():
+    #user: str = "facu1"
+    #password: str = "1234"
+    #host: str = "127.0.0.1"
+    #port: str = "3306"
+    #database: str = "CalculateCost"
+
+    try:
+        conn = mariadb.connect(
+            host = "localhost",
+            user = "facu1",
+            password = "1234",
+            port = 3306,
+            database = "CalculateCost"
+        )
+    except mariadb.Error as e:
+        print(f"Error en la conexion de base de datos: {e}")
+        sys.exit(1)
+    return conn
+
+def post_data(sql: str):
+    try:
+        connection: mariadb.Connection = connect()
+        connection.autocommit = False
+        cursor: mariadb.Cursor = connection.cursor()
+        cursor.execute(sql)
+    except mariadb.Error as e:
+        connection.rollback()
+        connection.close()
+        print(f"Error: {e}")
+    connection.commit()
+    print(f"Last inserted ID: {cursor.lastrowid}")
+    connection.close()
+    return True
+
+def get_data(sql: str):
+    try:
+        connection: mariadb.Connection = connect()
+        cursor: mariadb.Cursor = connection.cursor()
+        cursor.execute(sql)
+        #for (ID_user,Name,Balance,ID_group) in cursor:
+            #print(f"ID: {ID_user}, Name: {Name}, Balance: {Balance}, ID_group: {ID_group} ")
+    except mariadb.Error as e:
+        connection.close()
+        print(f"Error: {e}")
+    #return connection
+
+#print(post_data("INSERT INTO User(ID_user,Name,Balance,ID_group) values (3,'algo',231,2)"))
+#conn = get_data("SELECT * FROM User")
